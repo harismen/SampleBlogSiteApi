9a84711f6d168c1609e1f9297f3cc29a43e05b9a
Henrique Arismendi <harismen@gmail.com>
2024-02-14 17:31:40 +0100

    Added Serilog for Logging on Console and File

--- SampleBlogDatabaseLayer/Repository/BlogRepository.cs
+++ SampleBlogDatabaseLayer/Repository/BlogRepository.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking.Internal;
+using Microsoft.Extensions.Logging;
 using SampleBlogDatabaseCore;
 using SampleBlogDatabaseLayer.Interfaces;
 using SampleBlogModels.Models;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -37,14 +39,21 @@ namespace SampleBlogDatabaseLayer.Repository
 
                 var result = await _context.Database.ExecuteSqlRawAsync("EXEC InsertCategory @Name, @CreatedDate, @IsActive, @IsDeleted",
                                 nameParam, createdDateParam, isActiveParam, isDeletedParam);
+                if (result < 0) 
+                {
+                    Log.Error($"Something went wrong trying to insert Category. SP Result: {result}");
+                }
+                else
+                {
+                    Log.Information($"Successfully inserted Category {category.Name}.");
+                }
                 return result;
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Log.Error(ex.Message, "Database Error on Category insertion");
                 throw;
             }
-
         }
     }
 }
--- SampleBlogSiteApi/Program.cs
+++ SampleBlogSiteApi/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using SampleBlogBusinessLayer.Interfaces;
 using SampleBlogBusinessLayer.Services;
 using SampleBlogDatabaseCore;
+using Serilog;
 
 namespace SampleBlogSiteApi
 {
@@ -9,47 +10,69 @@ namespace SampleBlogSiteApi
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
 
-            // Add services to the container.
-            builder.Services.AddDbContext<SampleBlogDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SampleBlogDbConnection"));
-            });
+            Log.Information("Starting up");
+            try
+			{
+                var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddScoped<IBlogService, BlogService>();
+                // Configure Serilog
+                builder.Host.UseSerilog((ctx, lc) => lc
+                    .WriteTo.Console()
+                    .ReadFrom.Configuration(ctx.Configuration));
 
-            builder.Services.AddCors(o => {
-                o.AddPolicy("AllowAll", builder =>
-                    builder.AllowAnyOrigin()
-                    .AllowAnyMethod()
-                    .AllowAnyHeader()
-                    .WithExposedHeaders("Content-Disposition"));
-            });
+                // Add services to the container.
+                builder.Services.AddDbContext<SampleBlogDbContext>(options =>
+                {
+                    options.UseSqlServer(builder.Configuration.GetConnectionString("SampleBlogDbConnection"));
+                });
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+                builder.Services.AddScoped<IBlogService, BlogService>();
 
-            var app = builder.Build();
+                builder.Services.AddCors(o => {
+                    o.AddPolicy("AllowAll", builder =>
+                        builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("Content-Disposition"));
+                });
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                app.UseCors("AllowAll");
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                app.Run();
             }
-
-            app.UseCors("AllowAll");
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-            app.MapControllers();
-
-            app.Run();
+			catch (Exception ex)
+			{
+                Log.Fatal(ex, "Unhandled exception");
+			}
+            finally
+            {
+                Log.Information("Shut down complete");
+                Log.CloseAndFlush();
+            }
         }
     }
 }
